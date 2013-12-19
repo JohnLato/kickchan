f9aa2d4af3a83e4704b27e91e3ebaafd8f97a8cf
John Lato <jwlato@tsurucapital.com>
2013-12-18 22:56:20 -0800

    simplify checkWithPosition
    We only need to read the Position, not modify it.

--- src/Chan/KickChan.hs
+++ src/Chan/KickChan.hs
@@ -138,13 +138,13 @@ readyOrWait await readP p@(Position nextP pMap) =
 
 -- we know the value has been committed, we just need to check that it's still
 -- valid.
-checkWithPosition :: Int -> Int -> Position a -> (Position a, CheckResult)
-checkWithPosition sz readP pos@(Position nextP _pMap) =
+checkWithPosition :: Int -> Int -> Position a -> CheckResult
+checkWithPosition sz readP (Position nextP _pMap) =
   case nextP-readP of
       dif -- result should be ok.
-          | dif > 0 && dif <= sz -> (pos,Ok)
+          | dif > 0 && dif <= sz -> Ok
           -- requests that are too old or too far in the future
-          | otherwise -> (pos,Invalid)
+          | otherwise -> Invalid
 
 -- | A Channel that drops elements from the end when a 'KCReader' lags too far
 -- behind the writer.
@@ -211,9 +211,9 @@ getKickChan KickChan {..} readP = do
     if proceed -- value is definitely committed.
       then do
         x <- M.unsafeRead kcV (readP .&. kcSz)
+        checkedPos <- readIORef kcPos
         -- add 1 to kcSize because we store 1-size
-        result <- atomicModifyIORef' kcPos (checkWithPosition (kcSz+1) readP)
-        case result of
+        case checkWithPosition (kcSz+1) readP checkedPos of
             Ok    -> return $ Just x
             Invalid -> return Nothing
       else takeMVar await
